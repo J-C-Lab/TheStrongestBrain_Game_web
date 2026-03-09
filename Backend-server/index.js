7bc6e0aa301107bc3e718b766474cba580fd5bb8
CholeUnique <erhui443@gmail.com>
2026-03-09 15:46:54 +0800

    优化《羊了个羊》

--- Backend-server/index.js
+++ Backend-server/index.js
@@ -12,5 +12,6 @@ app.use('/api/badges', require('./routers/badges'));
 app.use('/api/life-game', require('./routers/game/lifeGame'));
 app.use('/api/precise-word', require('./routers/game/preciseWord'));
 app.use('/api/arrow-maze', require('./routers/game/arrowMaze'));
+app.use('/api/sheep-game',require('./routers/game/sheepGame'));
 
 app.listen(PORT, () => console.log(`🚀 高级后端服务器已启动：http://localhost:${PORT}`));
