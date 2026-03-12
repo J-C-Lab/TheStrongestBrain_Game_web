249a9c5067b7e23bc41872225b3d10d4deb46008
CholeUnique <erhui443@gmail.com>
2026-03-12 11:33:32 +0800

    完善《你画我猜》加分机制

--- src/components/DrawGuess.jsx
+++ src/components/DrawGuess.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import confetti from 'canvas-confetti';
 import {useNavigate } from 'react-router-dom';
 import { BASE_URL } from '../apiConfig';
